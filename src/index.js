54c3b709de497a384a68ab10951414371846ef1d
vedantgurav <40576802+vedantgurav@users.noreply.github.com>
2021-03-31 22:48:02 +0530

    Prettified

--- src/index.js
+++ src/index.js
@@ -1,33 +1,34 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 
-const slider = document.querySelector('.swatch-images');
+const slider = document.querySelector(".swatch-images");
 let isDown = false;
-let startX;
-let scrollLeft;
+let startX = 0;
+let scrollLeft = 0;
 
-slider.addEventListener('mousedown', (e) => {
-  isDown = true;
-  slider.classList.add('active');
-  startX = e.pageX - slider.offsetLeft;
-  scrollLeft = slider.scrollLeft;
+slider.addEventListener("mousedown", (e) => {
+    alert();
+    isDown = true;
+    slider.classList.add("active");
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
 });
-slider.addEventListener('mouseleave', () => {
-  isDown = false;
-  slider.classList.remove('active');
+slider.addEventListener("mouseleave", () => {
+    isDown = false;
+    slider.classList.remove("active");
 });
-slider.addEventListener('mouseup', () => {
-  isDown = false;
-  slider.classList.remove('active');
+slider.addEventListener("mouseup", () => {
+    isDown = false;
+    slider.classList.remove("active");
 });
-slider.addEventListener('mousemove', (e) => {
-  if(!isDown) return;
-  e.preventDefault();
-  const x = e.pageX - slider.offsetLeft;
-  const walk = (x - startX);
-  slider.scrollLeft = scrollLeft - walk;
+slider.addEventListener("mousemove", (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    const walk = x - startX;
+    slider.scrollLeft = scrollLeft - walk;
 });
